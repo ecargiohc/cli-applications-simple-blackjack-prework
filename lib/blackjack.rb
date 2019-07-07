8b31e403ad90402ce5b3d17576febd364dca3e06
Grace Colette <ilovegrace09@gmail.com>
2019-07-07 23:57:17 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,10 +33,10 @@ def hit?(card_total)
   prompt_user
   input = get_user_input
   if input == 'h'
-    deal_card 
+    deal_card + card_total
     
   end
-  return deal_card + card_total 
+  return card_total 
 end
 
 def invalid_command
