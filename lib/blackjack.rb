52e6b8e507d5189722cca0f5ea7fa8b283eaeb7d
Grace Colette <ilovegrace09@gmail.com>
2019-07-07 22:04:20 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,8 +7,8 @@ def deal_card
 end
 
 def display_card_total(card_total)
-  # print card_total
-  puts "Your cards add up to #{card_total}".
+  print card_total
+  # puts "Your cards add up to #{card_total}".
 end
 end
 
